fc7fa574a0f729194a0c2d003c3cb60d25631c41
Leticia Alves <letxizalves@gmail.com>
2026-03-27 16:38:31 -0400

    feat: adiciona base de dados de séries

---
+++ js/data/series.js
@@ -0,0 +1,5 @@
+export const series = {
+	Leticinha: ['TheOriginals.avf', 'TheVampireDiaries.jpg'],
+	Maria: ['Baby Shark.jpg', 'Lucas the Spider.jpg', 'Turma da Mônica.jpg'],
+	Evellyn: ['13 reasons why.avf', 'bungou stray dogs.jpg', 'fumetsu no anata.jpg', 'Hellsing.jpg', 'to your eternity.jpg']
+};
