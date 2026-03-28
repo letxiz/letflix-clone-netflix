6ba289bb09560b129cbc3df6280f47874b8b859c
Leticia Alves <letxizalves@gmail.com>
2026-03-27 20:49:25 -0400

    feat: ajustando index

--- js/paginas/index.js
+++ js/paginas/index.js
@@ -1,14 +1,5 @@
-import { obterPerfilAtivo } from '../components/perfil.js';
-
 function iniciarIndex() {
-	const perfilAtivo = obterPerfilAtivo();
-
-	if (!perfilAtivo) {
-		window.location.href = 'perfis.html';
-		return;
-	}
-
-	window.location.href = 'catalogo.html';
+	window.location.href = 'perfis.html';
 }
 
 document.addEventListener('DOMContentLoaded', iniciarIndex);
